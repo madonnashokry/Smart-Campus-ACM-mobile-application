3d8be02037f665dfbbb69ba73d9363bc77e51fab
madonnashokry <103499148+madonnashokry@users.noreply.github.com>
2024-03-30 22:38:50 +0200

    Front-end design

--- app/src/main/java/com/campus/acm/notfications.java
+++ app/src/main/java/com/campus/acm/notfications.java
@@ -3,11 +3,11 @@ package com.campus.acm;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.core.app.NotificationCompat;
 
 public class notfications extends AppCompatActivity {
-    public static final String CHANNEL_1_ID = "channel1";
-    public static final String CHANNEL_2_ID = "channel2";
+    public static final String CHANNEL_ID = "channel1";
+   // public static final String CHANNEL_2_ID = "channel2";
     private static final int PERMISSION_REQUEST_CODE = 101;
     private int NOTIFICATION_ID = 1;
     private String textTitle = "Test Notification";
@@ -19,6 +19,11 @@ public class notfications extends AppCompatActivity {
         // EdgeToEdge.enable(this);
         setContentView(R.layout.activity_notfications);
         // Request the POST_NOTIFICATIONS permission
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                //.setSmallIcon(R.drawable.notification_icon)
+                .setContentTitle(textTitle)
+                .setContentText(textContent)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
 
     }
 }
