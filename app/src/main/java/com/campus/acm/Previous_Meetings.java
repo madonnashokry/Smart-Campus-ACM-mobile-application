6e2835904065026375d37c5be5b99e66ae0cbb3c
madonnashokry <103499148+madonnashokry@users.noreply.github.com>
2024-04-12 16:05:40 +0200

    API`S interaction

--- app/src/main/java/com/campus/acm/Previous_Meetings.java
+++ app/src/main/java/com/campus/acm/Previous_Meetings.java
@@ -1,27 +1,16 @@
 package com.campus.acm;
 
 import android.os.Bundle;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-
-import java.io.IOException;
-import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
 import RecyclerView.EventsAdapter;
 import Session.Events;
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 
 
 public class Previous_Meetings extends AppCompatActivity {
@@ -40,11 +29,11 @@ public class Previous_Meetings extends AppCompatActivity {
         eventList = new ArrayList<>();
         eventAdapter = new EventsAdapter(eventList);
         recyclerView.setAdapter(eventAdapter);
-        fetchPreviousMeetings();
+       // fetchPreviousMeetings();
     }
    // fetchPreviousMeetings();
 
-
+     /*
     private void fetchPreviousMeetings() {
         OkHttpClient client = new OkHttpClient();
 
@@ -89,7 +78,7 @@ public class Previous_Meetings extends AppCompatActivity {
         Type listType = new TypeToken<List<Events>>(){}.getType();
         return gson.fromJson(responseBody, listType);
     }
-
+*/
 
 }
 
